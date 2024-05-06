85541dab7c63dc42ab9446dc327d04dc56ec2eed
Pasquale Gravante <pasquale.gravante@marconi.loc>
2024-05-06 08:08:43 +0200

    Progetto avviato

---
+++ src/main/java/it/gravante/gravante/GravanteApplication.java
@@ -0,0 +1,13 @@
+package it.gravante.gravante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GravanteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GravanteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/gravante/gravante/GravanteApplicationTests.java
@@ -0,0 +1,13 @@
+package it.gravante.gravante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GravanteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
